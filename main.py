894db4175d32fcb11829d965e6842ae687a8c0c3
Marcelo Júnior <marcelohjw@gmail.com>
2021-05-31 18:15:09 -0300

    Learnig more..

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ import PySimpleGUI as sg
 hours = sg.popup_get_text('Quantas horas você estudou hoje?')
 
 resu = int(hours) * 10
-
-sg.popup_annoying(str(resu))
+if resu < 20:
+    sg.popup_annoying('Preguiçoso! Estude mais!')
+else:
+    sg.popup_annoying('Muito bem! Continue nesse ritmo!')
